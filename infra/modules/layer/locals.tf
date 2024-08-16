cc5cb36efa1be9e613d45d89f73b6a416b2ec61d
massimobiagioli <biagiolimassimo@gmail.com>
2024-08-16 16:27:30 +0200

    add jwt dep

--- infra/modules/layer/locals.tf
+++ infra/modules/layer/locals.tf
@@ -4,6 +4,6 @@ locals {
   build_dir           = "${path.module}/../../../build/layer"
   layer_dir           = "${local.build_dir}/${var.name}"
   filename            = "${local.build_dir}/${var.name}.zip"
-  layer_s3_key        = "${var.name}-${filebase64sha256(local.pyproject_toml)}.zip"
+  layer_s3_key        = "${var.name}.zip"
   pyproject_toml      = "${path.module}/../../../pyproject.toml"
 }
