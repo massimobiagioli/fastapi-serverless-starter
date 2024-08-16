41daf3b7ac4a6980fbbc0ce3b73fc5a43b06a433
massimobiagioli <biagiolimassimo@gmail.com>
2024-08-16 16:06:23 +0200

    change layer tf

--- infra/modules/layer/locals.tf
+++ infra/modules/layer/locals.tf
@@ -4,6 +4,7 @@ locals {
   build_dir           = "${path.module}/../../../build/layer"
   layer_dir           = "${local.build_dir}/${var.name}"
   filename            = "${local.build_dir}/${var.name}.zip"
-  layer_s3_key        = "${var.name}.zip"
+  #layer_s3_key        = "${var.name}.zip"
+  layer_s3_key        = "${filebase64sha256(local.pyproject_toml)}-${var.name}.zip"
   pyproject_toml      = "${path.module}/../../../pyproject.toml"
 }
